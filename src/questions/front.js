a3a6d29955af51eeeebf155e3d2d09b9e94c1c7e
Marco Gigliarano <marco.gigliarano@sympla.com.br>
2019-08-22 09:32:18 -0300

    Add question front Marco

--- src/questions/front.js
+++ src/questions/front.js
@@ -26,4 +26,103 @@ export const front = [
       "photo": require("../assets/images/glauro.jpg")
     }
   },
+  {
+    "ask": "Qual é a resposta correta?",
+    "image": require("../assets/images/front/questionMarco1.png"),
+    "options": ["SyntaxError", "ReferenceError", "TypeError", "undefined"],
+    "correct": "TypeError",
+    "value": 2,
+    "author": {
+        "name": "Marco Gigliarano",
+      "photo": require("../assets/images/marco.png")
+     }
+  },
+  {
+    "ask": "Qual é a resposta correta?",
+    "image": require("../assets/images/front/questionMarco2.png"),
+    "options": ["[1, 2, 3, 4, 5]", "[1, 2, 3, 5]", "[1, 2, 3, 4]", "Error"],
+    "correct": "Error",
+    "value": 3,
+    "author": {
+        "name": "Marco Gigliarano",
+      "photo": require("../assets/images/marco.png")
+     }
+  },
+  {
+    "ask": "Qual é a resposta correta?",
+    "image": require("../assets/images/front/questionMarco3.png"),
+    "options": ["Sorry, you're too young.", "Yay! You're old enough!", "ReferenceError", "undefined"],
+    "correct": "ReferenceError",
+    "value": 1,
+    "author": {
+        "name": "Marco Gigliarano",
+      "photo": require("../assets/images/marco.png")
+     }
+  },
+  {
+    "ask": "Are CSS property names case-sensitive?",
+    "image": require("../assets/images/front/questionMarco4.png"),
+    "options": ["Yes", "No"],
+    "correct": "No",
+    "value": 1,
+    "author": {
+        "name": "Marco Gigliarano",
+      "photo": require("../assets/images/marco.png")
+     }
+  },
+  {
+    "ask": "Does the <bdo> tag change the direction of text?",
+    "image": "",
+    "options": ["Yes", "No"],
+    "correct": "Yes",
+    "value": 2,
+    "author": {
+        "name": "Marco Gigliarano",
+      "photo": require("../assets/images/marco.png")
+     }
+  },
+  {
+    "ask": "Does the HTML above trigger a http request when the page first loads?",
+    "image": require("../assets/images/front/questionMarco6.png"),
+    "options": ["Yes", "No"],
+    "correct": "Yes",
+    "value": 2,
+    "author": {
+        "name": "Marco Gigliarano",
+      "photo": require("../assets/images/marco.png")
+     }
+  },
+  {
+    "ask": "What does the above statement evaluate to in JS?",
+    "image": require("../assets/images/front/questionMarco7.png"),
+    "options": ["10", "4321","91"],
+    "correct": "91",
+    "value": 1,
+    "author": {
+        "name": "Marco Gigliarano",
+      "photo": require("../assets/images/marco.png")
+     }
+  },
+  {
+    "ask": "What is alerted?",
+    "image": require("../assets/images/front/questionMarco8.png"),
+    "options": ["1", "10","Function","undefined","Error"],
+    "correct": "1",
+    "value": 3,
+    "author": {
+        "name": "Marco Gigliarano",
+      "photo": require("../assets/images/marco.png")
+     }
+  },
+  {
+    "ask": "Qual é a definição Javascript para a abordagem abaixo?",
+    "image": require("../assets/images/front/caio1.png"),
+    "options": ["Encapsulation", "Hoisting", "Closure", "Nesting"],
+    "correct": "Closure",
+    "value": 2,
+    "author": {
+      "name": "Caio Silveira",
+      "photo": require("../assets/images/caio.png")
+    }
+ }
 ]
