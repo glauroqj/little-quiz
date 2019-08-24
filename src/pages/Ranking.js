c03af78c902e210141461f84970f8e4608ae500c
glauroqj <glauro.juliani@hotmail.com>
2019-08-23 22:54:14 -0300

    add: placeholders

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -16,6 +16,11 @@ import socksImg from '../assets/images/sock.png'
 import mugsImg from '../assets/images/mugs.png'
 import phoneImg from '../assets/images/phone.png'
 
+import ingressinhoPeso from '../assets/images/ingressinho-peso.png'
+import ingressinhoParaQuedas from '../assets/images/ingressinho-paraquedas.png'
+import ingressinhoTriste from '../assets/images/ingressinho-triste.png'
+import ingressinhoTriste2 from '../assets/images/ingressinho-triste2.png'
+
 const Ranking = props => {
   const { front, back, android, others, loading } = props.state.ranking
   const dispatch = useDispatch()
@@ -81,20 +86,23 @@ const Ranking = props => {
             <div className="col-sm-3">
               <h4 className="display-5 text-center">Ranking Front-End</h4>
 
-              {front.length > 0 && 
+              {front.length > 0 &&
                 front.map((item, index) => (
-                <ol key={`${item.player}${item.email}`} className="breadcrumb animated fadeInUp" title={`NAME: ${item.player} \nEMAIL: ${item.email} \nBOUNTY: ${item.bounty}`}>
-                  <span className="name">{index+1} {item.player}</span>
-                  {/* <span className="email">{item.email}</span> */}
-                  <span className="time">{item.time}</span>
-                  <span className="score badge badge-success badge-pill">
-                    {`${item.score} / 20`}
-                    <img className="ml-2 img-icon" src={chooseImg(item.bounty)} alt="bounty" />
-                  </span>
-                </ol>
-              ))
-              .filter((item, index) => index < 10)
+                  <ol key={`${item.player}${item.email}`} className="breadcrumb animated fadeInUp" title={`NAME: ${item.player} \nEMAIL: ${item.email} \nBOUNTY: ${item.bounty}`}>
+                    <span className="name">{index+1} {item.player}</span>
+                    {/* <span className="email">{item.email}</span> */}
+                    <span className="time">{item.time}</span>
+                    <span className="score badge badge-success badge-pill">
+                      {`${item.score} / 20`}
+                      <img className="ml-2 img-icon" src={chooseImg(item.bounty)} alt="bounty" />
+                    </span>
+                  </ol>
+                ))
+                .filter((item, index) => index < 10)
               }
+              {front.length === 0 && (
+                <img className="img-fluid" src={ingressinhoTriste2} alt="ingressinho" />
+              )}
             </div>
 
             <div className="col-sm-3">
@@ -102,18 +110,21 @@ const Ranking = props => {
 
               {back.length > 0 && 
                 back.map((item, index) => (
-                <ol key={`${item.player}${item.email}`} className="breadcrumb animated fadeInUp" title={`NAME: ${item.player} \nEMAIL: ${item.email} \nBOUNTY: ${item.bounty}`}>
-                  <span className="name">{index+1} {item.player}</span>
-                  {/* <span className="email">{item.email}</span> */}
-                  <span className="time">{item.time}</span>
-                  <span className="score badge badge-success badge-pill">
-                    {`${item.score} / 20`}
-                    <img className="ml-2 img-icon" src={chooseImg(item.bounty)} alt="bounty" />
-                  </span>
-                </ol>
-              ))
-              .filter((item, index) => index < 10)
+                  <ol key={`${item.player}${item.email}`} className="breadcrumb animated fadeInUp" title={`NAME: ${item.player} \nEMAIL: ${item.email} \nBOUNTY: ${item.bounty}`}>
+                    <span className="name">{index+1} {item.player}</span>
+                    {/* <span className="email">{item.email}</span> */}
+                    <span className="time">{item.time}</span>
+                    <span className="score badge badge-success badge-pill">
+                      {`${item.score} / 20`}
+                      <img className="ml-2 img-icon" src={chooseImg(item.bounty)} alt="bounty" />
+                    </span>
+                  </ol>
+                ))
+                .filter((item, index) => index < 10)
               }
+              {back.length === 0 && (
+                <img className="img-fluid" src={ingressinhoPeso} alt="ingressinho" />
+              )}
             </div>
 
             <div className="col-sm-3">
@@ -121,18 +132,21 @@ const Ranking = props => {
 
               {android.length > 0 && 
                 android.map((item, index) => (
-                <ol key={`${item.player}${item.email}`} className="breadcrumb animated fadeInUp" title={`NAME: ${item.player} \nEMAIL: ${item.email} \nBOUNTY: ${item.bounty}`}>
-                  <span className="name">{index+1} {item.player}</span>
-                  {/* <span className="email">{item.email}</span> */}
-                  <span className="time">{item.time}</span>
-                  <span className="score badge badge-success badge-pill">
-                    {`${item.score} / 20`}
-                    <img className="ml-2 img-icon" src={chooseImg(item.bounty)} alt="bounty" />
-                  </span>
-                </ol>
-              ))
-              .filter((item, index) => index < 10)
+                  <ol key={`${item.player}${item.email}`} className="breadcrumb animated fadeInUp" title={`NAME: ${item.player} \nEMAIL: ${item.email} \nBOUNTY: ${item.bounty}`}>
+                    <span className="name">{index+1} {item.player}</span>
+                    {/* <span className="email">{item.email}</span> */}
+                    <span className="time">{item.time}</span>
+                    <span className="score badge badge-success badge-pill">
+                      {`${item.score} / 20`}
+                      <img className="ml-2 img-icon" src={chooseImg(item.bounty)} alt="bounty" />
+                    </span>
+                  </ol>
+                ))
+                .filter((item, index) => index < 10)
               }
+              {android.length === 0 && (
+                <img className="img-fluid" src={ingressinhoTriste} alt="ingressinho" />
+              )}
             </div>
 
             <div className="col-sm-3">
@@ -140,18 +154,21 @@ const Ranking = props => {
 
               {others.length > 0 && 
                 others.map((item, index) => (
-                <ol key={`${item.player}${item.email}`} className="breadcrumb animated fadeInUp" title={`NAME: ${item.player} \nEMAIL: ${item.email} \nBOUNTY: ${item.bounty}`}>
-                  <span className="name">{index+1} {item.player}</span>
-                  {/* <span className="email">{item.email}</span> */}
-                  <span className="time">{item.time}</span>
-                  <span className="score badge badge-success badge-pill">
-                    {`${item.score} / 20`}
-                    <img className="ml-2 img-icon" src={chooseImg(item.bounty)} alt="bounty" />
-                  </span>
-                </ol>
-              ))
-              .filter((item, index) => index < 10)
+                  <ol key={`${item.player}${item.email}`} className="breadcrumb animated fadeInUp" title={`NAME: ${item.player} \nEMAIL: ${item.email} \nBOUNTY: ${item.bounty}`}>
+                    <span className="name">{index+1} {item.player}</span>
+                    {/* <span className="email">{item.email}</span> */}
+                    <span className="time">{item.time}</span>
+                    <span className="score badge badge-success badge-pill">
+                      {`${item.score} / 20`}
+                      <img className="ml-2 img-icon" src={chooseImg(item.bounty)} alt="bounty" />
+                    </span>
+                  </ol>
+                ))
+                .filter((item, index) => index < 10)
               }
+              {others.length === 0 && (
+                <img className="img-fluid" src={ingressinhoParaQuedas} alt="ingressinho" />
+              )}
             </div>
 
           </div>
